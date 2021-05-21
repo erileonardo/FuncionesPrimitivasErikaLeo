c7cc20559037affd5b9ac082ad514662f9989e3d
erileonardo <82676091+erileonardo@users.noreply.github.com>
2021-05-20 22:35:57 -0500

    Add files via upload

---
+++ pruebaIntegral2.m
@@ -0,0 +1,1 @@
+% Octave Script% Title       : Funciones Primitivas% Description : Integrar las funciones % Author      : Erika Amairani Leonardo Arce% Date        : 20 de Mayo 2021% Version     : 1% Notes       : funcion f(x)=3/x+5/(1+(x^2))clear allclcsyms tf=input('Ingresar funcion: ')inegral=int(f,t)
