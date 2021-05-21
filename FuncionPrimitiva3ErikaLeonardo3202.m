24031959259a242888a3772c45931ef60deec698
erileonardo <82676091+erileonardo@users.noreply.github.com>
2021-05-20 21:08:36 -0500

    Create FuncionPrimitiva3ErikaLeonardo3202.m

---
+++ FuncionPrimitiva3ErikaLeonardo3202.m
@@ -0,0 +1,13 @@
+% Octave Script
+% Title       : Funciones Primitivas
+% Description : Integrar las funciones 
+% Author      : Erika Amairani Leonardo Arce
+% Date        : 20 de Mayo 2021
+% Version     : 1
+% Notes       : funcion f(x)=5
+
+clear all
+clc
+syms x
+f=input('Ingresar funcion: ')
+inegral=int(f,x)
